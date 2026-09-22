69afc4983fca432fd7faf6799ccadbd795703c2c
MineDarinKA <egor.v.kolesnikov@gmail.com>
2026-09-22 20:07:29 +0300

    feat(input): add parameter validation

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,18 +1,65 @@
 ﻿#include <iostream>
+#include <sstream>
+#include <string>
 
 int main()
 {
-	int speed;
-	int length;
-	int epilepsyMode;
+    int speed;
+    int length;
+    char epilepsyMode;
 
+    std::string input;
 
-	std::cout << "Enter speed (1-30): ";
-	std::cin >> speed;
+    // Request and validate line speed.
+    while (true) {
+        std::cout << "Enter speed (1-30): ";
+        std::getline(std::cin, input);
 
-	std::cout << "Enter line length (1-30): ";
-	std::cin >> length;
+        std::stringstream stream(input);
 
-	std::cout << "Epilepsy mode (Y/N): ";
-	std::cin >> epilepsyMode;
+        if (stream >> speed) {
+            std::string remainingInput;
+
+            if (!(stream >> remainingInput) && speed >= 1 && speed <= 30) {
+                break;
+            }
+        }
+
+        std::cout << "Invalid speed. ";
+    }
+
+    // Request and validate line length.
+    while (true) {
+        std::cout << "Enter line length (1-30): ";
+        std::getline(std::cin, input);
+
+        std::stringstream stream(input);
+
+        if (stream >> length) {
+            std::string remainingInput;
+
+            if (!(stream >> remainingInput) && length >= 1 && length <= 30) {
+                break;
+            }
+        }
+
+        std::cout << "Invalid length. ";
+    }
+
+    // Request and validate epilepsy mode.
+    while (true) {
+        std::cout << "Epilepsy mode (Y/N): ";
+        std::getline(std::cin, input);
+
+        if (input.length() == 1 &&
+            (input[0] == 'Y' ||
+                input[0] == 'y' ||
+                input[0] == 'N' ||
+                input[0] == 'n')) {
+            epilepsyMode = input[0];
+            break;
+        }
+
+        std::cout << "Invalid mode. ";
+    }
 }
